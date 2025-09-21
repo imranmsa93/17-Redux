a9ec6429575a6f3b71981c16a75e9827ec22c88a
Muhammad Imran <miahmad@moi.gov.ae>
2025-09-22 00:25:41 +0400

    Redux libary added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './store/appStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+ root.render(<Provider store={store}> <App /> </Provider> );
+//root.render(<App /> );
---
+++ src/store/appStore.js
@@ -0,0 +1,20 @@
+import { createStore } from "redux";
+
+const initialState = { counter: 0 };
+const counterReducer = (state = initialState,action)=>{
+   if(action.type === 'increment'){
+      return {
+         counter: state+1
+        };
+    }
+     if(action.type === 'decrement'){
+      return {
+         counter: state+1
+        };
+    }
+   return state;
+   
+}
+const store = createStore(counterReducer);
+
+export default store;
